5b0520761a7ff877c561c3034b43bc623e6d4801
PaulaCostaOliveira <paulavanessaco@gmail.com>
2022-02-09 20:50:03 -0300

    Atualizando o repositorio de exercicios de vetores, arrays e lista

---
+++ src/com/paula/estruturadados/vetor/exercicios/Ex04.java
@@ -0,0 +1,20 @@
+package com.paula.estruturadados.vetor.exercicios;
+
+import com.paula.estruturadados.vetor.Lista;
+
+public class Ex04 {
+    public static void main(String[] args) {
+
+        Lista<String> lista = new Lista<String>(5);
+
+        lista.adiciona("A");
+        lista.adiciona("B");
+        lista.adiciona("C");
+        lista.adiciona("D");
+        lista.adiciona("E");
+
+        System.out.println(lista.obtem(0));
+        System.out.println(lista.obtem(2));
+        System.out.println(lista.obtem(4));
+    }
+}
